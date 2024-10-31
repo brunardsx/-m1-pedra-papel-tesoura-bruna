a166ade0260e73758e44b80f92f490d91375ca95
Bruna Antunes <brunaantunesrds@gmail.com>
2024-10-31 13:37:33 -0300

    função em js adicionada

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 /* Desenvolva seu código abaixo */
+
+
+function playRockPaperScissor (player1, player2){
+
+    if (
+        (player1 == 'Pedra' && player2 == 'Tesoura') ||
+        (player1 == 'Tesoura' && player2 == 'Papel') ||
+        (player1 == 'Papel' && player2 == 'Pedra')
+    ) {
+        return 'Jogador 1 venceu!'
+    } else if (
+        (player1 == 'Tesoura' && player2 == 'Pedra') ||
+        (player1 == 'Papel' && player2 == 'Tesoura') ||
+        (player1 == 'Pedra' && player2 == 'Papel')
+    ) {
+        return 'Jogador 2 venceu!'
+    } else {
+        return 'Empate!'
+    }
+}
+
+const resultado = playRockPaperScissor('Tesoura', 'Tesoura')
+console.log(resultado)
+
+
